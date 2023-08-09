dd48b431c8e39b9e8824e52a633ea94a8148ce41
bangProgram <40655921+bangProgram@users.noreply.github.com>
2023-08-09 21:45:46 +0900

    flutter tiktok review init
    플러터 tiktok 클론코딩 복습 초기 commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(scaffoldBackgroundColor: Colors.white),
+      home: const Scaffold(),
+    );
+  }
+}
